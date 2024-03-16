21441547e734833cad6240796ff5d775a53fc7b7
aqmn <adskeeper@aol.com>
2024-03-16 01:39:28 -0400

    cmd

---
+++ FK Codes _ Terminal_files/commands.js
@@ -0,0 +1,84 @@
+var youtube = "https://www.youtube.com/fknight/";
+var twitter = "https://www.twitter.com/uygniluohz/";
+var password = "hahaitsnull";
+var linkedin = "https://www.linkedin.com/in/lingyuzhou/";
+var instagram = "https://www.instagram.com/toqurdn/";
+var github = "https://github.com/xolyn/";
+var email = 'mailto:zly@dr.com';
+var gui = 'https://zhoulingyu.net';
+var portfolio= "https://sites.google.com/view/zhoulingyu/home";
+var webdav= "https://drive.zhoulingyu.net"
+
+whois = [
+
+  "I am Lingyu Zhou. I am the founder of <a href='https://khentmt.com'>KH Entertainment</a>,",
+  "currently studying applied statistics and data science at <a href='https://cornell.edu'>Cornell University</a> for my master's degree.",
+  "Previously, I studied applied mathematics at <a href='https://wisc.edu'>University of Wisconsin-Madison</a> for my bachelor's degree. ",
+"My research directions are nonlinear system, applied mathematical modeling, and applied statistics.",
+"My specialities are software engineering, math, data analysis, and data visualization.",
+"I am also interested in biology, botany, and linguistics.",
+"If you are interested, see my previous previous work <a href='https://sites.google.com/view/prevcs/home'>here</a>.",
+"Previously I worked at the Technology Strategy Department in <a href='http://m.hoperun.com/news.html'>Jiangsu Hoperun Software Co. Ltd.</a>", 
+"where my main work involves data analysis on Apache Flink platform and IoT complex events processing based on MQTT protocol. ",
+"More information, including the source code about this program can be found <a href='https://github.com/xolyn/flinkmqtt'>here</a>.",
+"Before joining Hoperun, I was a environmental data analyst at <a href='https://www.sino-technology.com/'>Shanghai Sino Environmental Technology Co., Ltd.</a>."
+];
+
+whoami = [
+
+  "WTF? How am I suppose to know.",
+
+];
+
+social = [
+
+  'youtube        <a href="' + youtube + '" target="_blank">youtube/fknight' + "</a>",
+  'twitter        <a href="' + twitter + '" target="_blank">twitter/forrestpknight' + '</a>',
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/forrestpknight' + "</a>",
+  'instagram      <a href="' + instagram + '" target="_blank">instagram/forrestpknight' + '</a>',
+  'github         <a href="' + github + '" target="_blank">github/forrestknight' + "</a>",
+
+];
+
+secret = [
+
+  '<span class="command">sudo</span>           Only use if you\'re admin'
+
+];
+
+projects = [
+
+  "Still curating... most projects are offline, on GitHub, or confidential."
+
+];
+
+help = [
+
+  '<span class="command">about</span>           Who am I?',
+  // '<span class="command">video</span>          View YouTube videos',
+  '<span class="command">social</span>          Display social networks',
+  // '<span class="command">secret</span>         Find the password',
+  '<span class="command">portfolio</span>       View past works',
+  '<span class="command">history</span>         View command history',
+  '<span class="command">help</span>            You obviously already know what this does',
+  '<span class="command">email</span>           SMTP!',
+  '<span class="command">clear</span>           Clear terminal',
+  '<span class="command">gui</span>             Not familiar to CLI? Okay..',
+  '<span class="command">webdav</span>          The world is your oyster!',
+];
+
+
+// banner=[
+// "<pre>",
+// "._     _                           _______                 ",
+// "| |   (_)                         |___  | |                ",
+// "| |    _ _ __   __ _ _   _ _   _     / /| |__   ___  _   _ ",
+// "| |   | | '_ \\ / _` | | | | | | |   / / | '_ \\ / _ \\| | | |",
+// "| |___| | | | | (_| | |_| | |_| | ./ /__| | | | (_) | |_| |",
+// "\\_____|_|_| |_|\\__, |\\__, |\\__,_| \\_____|_| |_|\\___/ \\__,_|",
+// "                __/ | __/ |                                ",
+// "               |___./|___./                                 ",
+// "</pre>"
+// ]
+
+
